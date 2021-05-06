764b87b529b991824ef7851c7fff1f9b4f997d44
Niklas Sabel <niklas.sabel@comm-uni.com>
2021-05-06 10:50:45 +0200

    Generate comparative features -> absolute, relative, normalized and drop absolute columns of single language features afterwards

--- src/data/clean_data.py
+++ src/data/clean_data.py
@@ -1,25 +1,42 @@
+import numpy as np
 from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
 
 
-def data_cleansing(df_selected):
+def data_cleansing(df):
     # tokenize the sentences
-    df_selected['English'] = df_selected.apply(lambda x: word_tokenize(x['English']), axis=1)
-    df_selected['German'] = df_selected.apply(lambda x: word_tokenize(x['German'], language='german'), axis=1)
-    # lower case
-    df_selected['English'] = df_selected['English'].apply(lambda row: list(map(str.lower, row)))
-    df_selected['German'] = df_selected['German'].apply(lambda row: list(map(str.lower, row)))
-    # treat stopwords in english
-    stop = stopwords.words('english')
-    # count stopwords in english
-    df_selected['SWords_eng'] = df_selected['English'].apply(lambda x: len([word for word in x if word in stop]))
-    # remove stopwords in english
-    df_selected['English'] = df_selected['English'].apply(lambda x: [word for word in x if word not in stop])
-    # treat stopwords in german
-    stop = stopwords.words('german')
-    # count stopwords in german
-    df_selected['SWords_ger'] = df_selected['German'].apply(lambda x: len([word for word in x if word in stop]))
-    # remove stopwords in german
-    df_selected['German'] = df_selected['German'].apply(lambda x: [word for word in x if word not in stop])
-    return df_selected
+    df['English'] = df.apply(lambda x: word_tokenize(x['English']), axis=1)
+    df['German'] = df.apply(lambda x: word_tokenize(x['German'], language='german'), axis=1)
 
+    # lower case
+    df['English'] = df['English'].apply(lambda row: list(map(str.lower, row)))
+    df['German'] = df['German'].apply(lambda row: list(map(str.lower, row)))
+
+    # treat stopwords in english
+    stop_eng = stopwords.words('english')
+    # count stopwords in english
+    df['SWords_eng'] = df['English'].apply(lambda x: len([word for word in x if word in stop_eng]))
+    # treat stopwords in german
+    stop_ger = stopwords.words('german')
+    # count stopwords in german
+    df['SWords_ger'] = df['German'].apply(lambda x: len([word for word in x if word in stop_ger]))
+
+    # get difference between stopwords absolute, relative with respect to english as base and
+    # normalized and drop the lines afterwards
+    df['SWords_dif'] = df['SWords_ger'] - df['SWords_eng']
+    df['SWords_dif_rel'] = df['SWords_dif'] / df['SWords_eng']
+    # set NaNs to zero
+    df['SWords_dif_rel'] = df['SWords_dif_rel'].replace(np.nan, 0)
+    # get a normalized difference by dividing the with whole length of preprocessed sentence to account for different
+    # lengths
+    df['SWords_dif_normal'] = (df['SWords_ger'] / df['German'].apply(lambda x: len(x))) - (
+            df['SWords_eng'] / df['English'].apply(lambda x: len(x)))
+    # drop columns with absolute values since we only want comparative features in our model
+    df = df.drop(columns=['SWords_ger', 'SWords_eng'])
+
+    # remove stopwords in german
+    df['German'] = df['German'].apply(lambda x: [word for word in x if word not in stop_ger])
+    # remove stopwords in english
+    df['English'] = df['English'].apply(lambda x: [word for word in x if word not in stop_eng])
+
+    return df
--- src/features/build_features.py
+++ src/features/build_features.py
@@ -6,44 +6,64 @@ import string
 
 
 def feature_generation(df):
+    """ Function to generate comparative features. """
     # get number of punctuation marks as feature, but drop the end of senctence points
     list_pm = list(string.punctuation)
     list_pm.remove('.')
     list_pm.append('...')
     df['PM_eng'] = df['English'].apply(lambda x: len([word for word in x if word in list_pm]))
     df['PM_ger'] = df['German'].apply(lambda x: len([word for word in x if word in list_pm]))
-
-    # get differences of punctuation marks absolute and relative with respect to smaller number as base
-    df['PM_dif_abs'] = np.abs(df['PM_eng'] - df['PM_ger'])
-    df['PM_dif_rel'] = df['PM_dif_abs'] / np.minimum(df['PM_eng'], df['PM_ger'])
+    # get differences of punctuation marks absolute and relative with respect to english as base
+    df['PM_dif'] = df['PM_ger'] - df['PM_eng']
+    df['PM_dif_rel'] = df['PM_dif'] / df['PM_eng']
     # set NaNs to zero
     df['PM_dif_rel'] = df['PM_dif_rel'].replace(np.nan, 0)
+    # get a normalized difference by dividing the with whole length of preprocessed sentence to account for different
+    # lengths
+    df['PM_dif_normal'] = (df['PM_ger'] / df['German'].apply(lambda x: len(x))) - (
+            df['PM_eng'] / df['English'].apply(lambda x: len(x)))
 
     # get number of words as feature
     df['Words_eng'] = df['English'].apply(
         lambda x: len([word for word in x if word not in string.punctuation]))
     df['Words_ger'] = df['German'].apply(
         lambda x: len([word for word in x if word not in string.punctuation]))
-
-    # get differences of words absolute and relative with respect to smaller number as base
-    df['Words_dif_abs'] = np.abs(df['Words_eng'] - df['Words_ger'])
-    df['Words_dif_rel'] = df['Words_dif_abs'] / np.minimum(df['Words_eng'], df['Words_ger'])
+    # get differences of words absolute and relative with respect to english as base
+    df['Words_dif'] = df['Words_ger'] - df['Words_eng']
+    df['Words_dif_rel'] = df['Words_dif'] / df['Words_eng']
     # set NaNs to zero
     df['Words_dif_rel'] = df['Words_dif_rel'].replace(np.nan, 0)
+    # get a normalized difference by dividing the with whole length of preprocessed sentence to account for different
+    # lengths
+    df['Words_dif_normal'] = (df['Words_ger'] / df['German'].apply(lambda x: len(x))) - (
+            df['Words_eng'] / df['English'].apply(lambda x: len(x)))
 
     # get number of unique words
     df['Words_eng_unique'] = df['English'].apply(
         lambda x: len(np.unique([word for word in x if word not in string.punctuation])))
     df['Words_ger_unique'] = df['German'].apply(
         lambda x: len(np.unique([word for word in x if word not in string.punctuation])))
+    # get differences of words absolute and relative with respect to english as base
+    df['Words_unique_dif'] = df['Words_ger_unique'] - df['Words_eng_unique']
+    df['Words_unique_dif_rel'] = df['Words_unique_dif'] / df['Words_eng_unique']
+    # set NaNs to zero
+    df['Words_unique_dif_rel'] = df['Words_unique_dif_rel'].replace(np.nan, 0)
+    # get a normalized difference by dividing the with whole length of preprocessed sentence to account for different
+    # lengths
+    df['Words_unique_dif_normal'] = (df['Words_ger_unique'] / df['German'].apply(lambda x: len(x))) - (
+            df['Words_eng_unique'] / df['English'].apply(lambda x: len(x)))
 
-    # get number of different punctuation marks without the end of sentence point
+    # get number of all different punctuation marks without the end of sentence point
     list_pm = list(string.punctuation)
     list_pm.remove('.')
     list_pm.append('...')
     for mark in list_pm:
-        df[f'en_{mark}'] = df['English'].apply(lambda x: len([word for word in x if word == mark]))
+        df[f'eng_{mark}'] = df['English'].apply(lambda x: len([word for word in x if word == mark]))
         df[f'ger_{mark}'] = df['German'].apply(lambda x: len([word for word in x if word == mark]))
+        # get differences absolute and relative with base of english
+        df[f'dif_{mark}'] = df[f'ger_{mark}'] - df[f'eng_{mark}']
+        # drop absolute values since we only want comparative features in our model
+        df = df.drop(columns=[f'eng_{mark}', f'ger_{mark}'])
 
     # get number of characters in words and the average char per word
     df['char_eng'] = df['English'].apply(
@@ -52,10 +72,15 @@ def feature_generation(df):
     df['char_ger'] = df['German'].apply(
         lambda x: len(str([word for word in x if word not in string.punctuation])))
     df['char_ger_avg'] = df['char_ger'] / df['Words_ger']
-    # absolute difference between avg
-    df['char_avg_abs'] = df['char_ger_avg'] - df['char_eng_avg']
+    # absolute difference between characters and relative with respect to english as base
+    df['char_dif'] = df['char_ger'] - df['char_eng']
+    df['char_dif_rel'] = df['char_dif'] / df['char_eng']
+    # set NaNs to zero
+    df['char_dif_rel'] = df['char_dif_rel'].replace(np.nan, 0)
+    # absolute difference between avg and relative with respect to english as base
+    df['char_dif_avg'] = df['char_ger_avg'] - df['char_eng_avg']
 
-    # use pos-tagger and get number of nouns, verbs, adjectives, tagset universal to only get the highest categories and
+    # use pos-tagger and get number of nouns, verbs, adjectives, tagset universal to only get the categories
     df['English_pos'] = df.apply(lambda x: nltk.pos_tag(x['English'], tagset='universal'), axis=1)
     # pos-tagging with nltk not suppored in german yet -> use pretrained model for german as tagger
     # pretrained model for german as tagger
@@ -71,7 +96,7 @@ def feature_generation(df):
     # get english tags and one hot encode them
     universal_pos = ['ADJ', 'ADP', 'ADV', 'CONJ', 'DET', 'NOUN', 'NUM', 'PRT', 'PRON', 'VERB', 'X']
     for u_tag in universal_pos:
-        df[f'en_{u_tag}'] = df['English_pos'].apply(
+        df[f'eng_{u_tag}'] = df['English_pos'].apply(
             lambda row: nltk.FreqDist(tag for (word, tag) in row if tag == u_tag)[u_tag])
 
     # Stuttgart, Tübingen Tagset
@@ -100,15 +125,37 @@ def feature_generation(df):
                      + df['ger_PPOSS'] + df['ger_PPOSAT'] + df['ger_PRELS'] + df['ger_PRELAT'] + df['ger_PRF'] \
                      + df['ger_PWS'] + df['ger_PWAT'] + df['ger_PWAV'] + df['ger_PAV']
     df['ger_VERB'] = df['ger_VVFIN'] + df['ger_VVIMP'] + df['ger_VVINF'] + \
-                              df['ger_VVIZU'] + df['ger_VVPP'] + df['ger_VAFIN'] + \
-                              df['ger_VAIMP'] + df['ger_VAINF'] + df['ger_VAPP'] + \
-                              df['ger_VMFIN'] + df['ger_VMINF'] + df['ger_VMPP']
+                     df['ger_VVIZU'] + df['ger_VVPP'] + df['ger_VAFIN'] + \
+                     df['ger_VAIMP'] + df['ger_VAINF'] + df['ger_VAPP'] + \
+                     df['ger_VMFIN'] + df['ger_VMINF'] + df['ger_VMPP']
     df['ger_X'] = df['ger_FM'] + df['ger_ITJ'] + df['ger_TRUNC'] + df[
         'ger_XY']
 
-    # #delete german tag columns without the Adverb cause the column name and the column itself do not get changed
-    # tags_german_1=['ADJA','ADJD','APPR','APPRART','APPO','APZR','ART','CARD','FM','ITJ','KOUI','KOUS','KON','KOKOM','NN','NE','PDS','PDAT','PIS','PIAT','PIDAT','PPER','PPOSS','PPOSAT','PRELS','PRELAT','PRF','PWS','PWAT','PWAV','PAV','PTKZU','PTKNEG','PTKVZ','PTKANT','PTKA','TRUNC','VVFIN','VVIMP','VVINF','VVIZU','VVPP','VAFIN','VAIMP','VAINF','VAPP','VMFIN','VMINF','VMPP','XY']
-    # for u_tag in tags_german_1:
-    #     del df[f'ger_{u_tag}']
+    # delete german tag columns without the Adverb cause the column name and the column itself do not get changed
+    tags_german_1 = ['ADJA', 'ADJD', 'APPR', 'APPRART', 'APPO', 'APZR', 'ART', 'CARD', 'FM', 'ITJ', 'KOUI', 'KOUS',
+                     'KON', 'KOKOM', 'NN', 'NE', 'PDS', 'PDAT', 'PIS', 'PIAT', 'PIDAT', 'PPER', 'PPOSS', 'PPOSAT',
+                     'PRELS', 'PRELAT', 'PRF', 'PWS', 'PWAT', 'PWAV', 'PAV', 'PTKZU', 'PTKNEG', 'PTKVZ', 'PTKANT',
+                     'PTKA', 'TRUNC', 'VVFIN', 'VVIMP', 'VVINF', 'VVIZU', 'VVPP', 'VAFIN', 'VAIMP', 'VAINF', 'VAPP',
+                     'VMFIN', 'VMINF', 'VMPP', 'XY']
+    for u_tag in tags_german_1:
+        del df[f'ger_{u_tag}']
+
+    # get differences in tags
+    for u_tag in universal_pos:
+        df[f'dif_{u_tag}'] = df[f'ger_{u_tag}'] - df[f'eng_{u_tag}']
+        # delete absolute columns since we only want comparative features
+        del df[f'eng_{u_tag}']
+        del df[f'ger_{u_tag}']
+
+    # drop columns with absolute values since we only want comparative features in our model
+    df = df.drop(columns=['PM_eng', 'PM_ger'])
+    # drop columns with absolute values since we only want comparative features in our model
+    df = df.drop(columns=['Words_eng', 'Words_ger'])
+    # drop columns with absolute values since we only want comparative features in our model
+    df = df.drop(columns=['Words_eng_unique', 'Words_ger_unique'])
+    # drop columns with absolute values since we only want comparative features in our model
+    df = df.drop(columns=['char_eng', 'char_ger'])
+    # drop columns with absolute values since we only want comparative features in our model
+    df = df.drop(columns=['char_eng_avg', 'char_ger_avg'])
 
     return df
