fde886e75be7cd23229cd0982af4b89ebcd992a0
Jakob <accounts@jakoblangenbahn.com>
2021-05-19 17:13:46 +0200

    Update feature generation

---
+++ src/features/embed_based.py
@@ -0,0 +1,10 @@
+""" Functions to work with the word embeddings
+"""
+
+import numpy as np
+
+
+def sentence_embedding(token_vector, embedding_matrix_path, embedding_dictionary_path):
+        embedding_sentence = np.delete(embedding_sentence, deletion_list, axis=0)
+        embedding_sentence_mean = np.mean(embedding_sentence, axis=0)
+        return embedding_sentence_mean
