521009ba4d907e7feea47760ddf8ff4b46335004
Niklas Sabel <niklas.sabel@comm-uni.com>
2021-05-19 17:03:35 +0200

    feature bug fixes

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -6,9 +6,10 @@ def get_sample(df, n):
     """ Function to generate a sample set of 2n with n correct examples and n wrong examples. """
     # get n entries with a correct translation marked with a 1 in the translation column
     df_right = pd.concat([df.loc[0:(n - 1)], pd.DataFrame(np.ones((n, 1)), columns=['Translation'])], axis=1)
-    # get n entries with a wrong translation marked with a 0 in the translation column
+    # get n entries with a wrong translation marked with a 0 in the translation column, shift of 1000 to prevent
+    # correlation
     df_wrong = pd.concat([pd.DataFrame(df.loc[n:(2 * n - 1)]['English']).reset_index(drop=True),
-                          pd.DataFrame(df.loc[(n + 1):2 * n]['German']).reset_index(drop=True)], axis=1)
+                          pd.DataFrame(df.loc[(n + 1000):2 * n+1001]['German']).reset_index(drop=True)], axis=1)
     df_wrong = pd.concat([df_wrong, pd.DataFrame(np.zeros((n, 1)), columns=['Translation'])], axis=1)
     # combine the two tables
     df_dataset = pd.concat([df_right, df_wrong]).reset_index(drop=True)
--- src/data/preprocessing_class.py
+++ src/data/preprocessing_class.py
@@ -3,9 +3,9 @@
 import string
 
 from src.data.import_data import import_data
-from src.data.preprocess_data import create_cleaned_token, number_punctuations_total, number_words, \
+from src.data.preprocess_data import create_cleaned_token, create_cleaned_text, number_punctuations_total, number_words, \
     number_unique_words, number_punctuation_marks, number_characters, number_pos, number_times, polarity, subjectivity, \
-    number_stopwords, named_entities, sentence_embedding
+    number_stopwords, named_entities, sentence_embedding, remove_stopwords
 
 
 class PreprocessingEuroParl:
@@ -22,14 +22,19 @@ class PreprocessingEuroParl:
                                     embedding_dictionary_source):
         self.dataframe["token_preprocessed_source"] = create_cleaned_token((self.dataframe["text_source"]),
                                                                            nlp_source, stopwords_source)
-        self.dataframe["number_punctuations_total_source"] = number_punctuations_total(self.dataframe["text_source"])
-        self.dataframe["number_words_source"] = number_words(self.dataframe["text_source"])
-        self.dataframe["number_unique_words_source"] = number_unique_words(self.dataframe["text_source"])
+        self.dataframe["text_source_1"] = create_cleaned_text(self.dataframe["text_source"])
+        # count stopwords before removing
+        self.dataframe["number_stopwords_source"] = number_stopwords(self.dataframe["text_source_1"],
+                                                                     stopwords_source)
+        self.dataframe["text_source_1"] = remove_stopwords(self.dataframe["text_source_1"], stopwords_source)
+        self.dataframe["number_punctuations_total_source"] = number_punctuations_total(self.dataframe["text_source_1"])
+        self.dataframe["number_words_source"] = number_words(self.dataframe["text_source_1"])
+        self.dataframe["number_unique_words_source"] = number_unique_words(self.dataframe["text_source_1"])
+        self.dataframe["number_characters_source"] = number_characters(self.dataframe["text_source_1"])
         for punctuation_mark in self.punctuation_list:
             self.dataframe[f"number_{punctuation_mark}_source"] = number_punctuation_marks(self.dataframe[
-                                                                                               "text_source"],
+                                                                                               "text_source_1"],
                                                                                            punctuation_mark)
-        self.dataframe["number_characters_source"] = number_characters(self.dataframe["text_source"])
         for pos in self.pos_list:
             self.dataframe[f"number_{pos}_source"] = number_pos(self.dataframe["text_source"], nlp_source, pos)
         for tense in self.tense_list:
@@ -39,8 +44,6 @@ class PreprocessingEuroParl:
         self.dataframe["score_polarity_source"] = polarity(self.dataframe["text_source"], textblob_source)
         self.dataframe["score_subjectivity_source"] = subjectivity(self.dataframe["text_source"],
                                                                    textblob_source)
-        self.dataframe["number_stopwords_source"] = number_stopwords(self.dataframe["text_source"],
-                                                                     stopwords_source)
         self.dataframe["list_named_entities_source"] = named_entities(self.dataframe["text_source"], nlp_source)
         self.dataframe["sentence_embedding_source"] = sentence_embedding(self.dataframe["token_preprocessed_source"],
                                                                          embedding_matrix_source,
@@ -50,14 +53,18 @@ class PreprocessingEuroParl:
                                     embedding_dictionary_target):
         self.dataframe["token_preprocessed_target"] = create_cleaned_token((self.dataframe["text_target"]),
                                                                            nlp_target, stopwords_target)
-        self.dataframe["number_punctuations_total_target"] = number_punctuations_total(self.dataframe["text_target"])
-        self.dataframe["number_words_target"] = number_words(self.dataframe["text_target"])
-        self.dataframe["number_unique_words_target"] = number_unique_words(self.dataframe["text_target"])
+        self.dataframe["text_target_1"] = create_cleaned_text(self.dataframe["text_target"])
+        self.dataframe["number_stopwords_target"] = number_stopwords(self.dataframe["text_target_1"],
+                                                                     stopwords_target)
+        self.dataframe["text_target_1"] = remove_stopwords(self.dataframe["text_target_1"], stopwords_target)
+        self.dataframe["number_punctuations_total_target"] = number_punctuations_total(self.dataframe["text_target_1"])
+        self.dataframe["number_words_target"] = number_words(self.dataframe["text_target_1"])
+        self.dataframe["number_unique_words_target"] = number_unique_words(self.dataframe["text_target_1"])
+        self.dataframe["number_characters_target"] = number_characters(self.dataframe["text_target_1"])
         for punctuation_mark in self.punctuation_list:
             self.dataframe[f"number_{punctuation_mark}_target"] = number_punctuation_marks(self.dataframe[
-                                                                                               "text_target"],
+                                                                                               "text_target_1"],
                                                                                            punctuation_mark)
-        self.dataframe["number_characters_target"] = number_characters(self.dataframe["text_target"])
         for pos in self.pos_list:
             self.dataframe[f"number_{pos}_target"] = number_pos(self.dataframe["text_target"], nlp_target, pos)
         for tense in self.tense_list:
@@ -67,8 +74,6 @@ class PreprocessingEuroParl:
         self.dataframe["score_polarity_target"] = polarity(self.dataframe["text_target"], textblob_target)
         self.dataframe["score_subjectivity_target"] = subjectivity(self.dataframe["text_target"],
                                                                    textblob_target)
-        self.dataframe["number_stopwords_target"] = number_stopwords(self.dataframe["text_target"],
-                                                                     stopwords_target)
         self.dataframe["list_named_entities_target"] = named_entities(self.dataframe["text_target"], nlp_target)
         self.dataframe["sentence_embedding_target"] = sentence_embedding(self.dataframe["token_preprocessed_target"],
                                                                          embedding_matrix_target,
