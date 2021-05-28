e3fdb229b9a39304eddb0353f506f23635439d5d
Jakob <accounts@jakoblangenbahn.com>
2021-05-28 23:37:08 +0200

    Add unsupervised model

---
+++ src/models/unsupervised_model.py
@@ -0,0 +1,13 @@
+""" Functions to train a unsupervised retrieval model
+
+TODO: Build unsupervised model with cosine similarity
+TODO: Build unsupervised model with euclidean distance
+TODO: Build unsupervised model with word mover distance
+"""
+
+
+def unsupervised_retrieval(dataframe, id_source, k):
+    df_filter = dataframe[dataframe.id_source.eq(id_source)]
+    df_sorted = df_filter.sort_values("cosine_similarity_average", axis=1)
+    df_sorted_k = df_sorted.iloc[:k]
+    return df_sorted_k.id_target
