f61d4cbf08d5c4a5aaa7ef1d5e9f04a40c563a93
Jakob <accounts@jakoblangenbahn.com>
2021-06-01 20:46:09 +0200

    Update init.py files

--- src/features/__init__.py
+++ src/features/__init__.py
@@ -1,8 +1,13 @@
 """
-.. automodule:: src.features.sentence_based
-    :members:
 .. automodule:: src.features.embed_based
     :members:
-.. automodule:: src.data.feature_generation
+.. automodule:: src.data.feature_generation_class
+    :members:
+.. automodule:: src.features.sentence_based
     :members:
 """
+
+from .embed_based import cosine_similarity_vector, euclidean_distance_vector, word_mover_distance_vector, jaccard, \
+    embedding_difference
+from .feature_generation_class import FeatureGeneration
+from .sentence_based import difference_numerical, relative_difference_numerical, normalized_difference_numerical
