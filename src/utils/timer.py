a514d8490b7d9346ea509f2c3076ef6a26b2f445
Jakob <accounts@jakoblangenbahn.com>
2021-06-01 17:22:09 +0200

    Add timer module

---
+++ src/utils/timer.py
@@ -0,0 +1,23 @@
+""" Function to show time neeed for the evaluation of a funciton.
+"""
+
+import time
+
+import functools
+
+
+def timer(function):
+    """ Function to show time neeed for the evaluation of a funciton.
+    """
+    @functools.wraps(function)
+    def time_measurement(*args, **kwargs):
+        """ Function to measure time elapsed by executing a function.
+        """
+        time_start = time.perf_counter()
+        result = function(*args, **kwargs)
+        time_end = time.perf_counter()
+        time_elapsed = time_end - time_start
+        print("Finished function: {} in {} seconds.".format(repr(function.__name__), round(time_elapsed, 2)))
+        return result
+
+    return time_measurement
