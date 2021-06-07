e2a4cd5114d793d3c7c8aec2a4ee67583b8329fe
I534344 <minh.duc.bui@sap.com>
2021-06-07 16:23:26 +0200

    all features notebook

--- src/models/predict_model.py
+++ src/models/predict_model.py
@@ -1,5 +1,6 @@
 import pandas as pd
-
+import numpy as np
+from sklearn.utils import shuffle
 
 def MAP_score(source_id, target_labels, prediction):
     """ Function to compute the Mean Average Precision score of a given ranking.
@@ -18,7 +19,7 @@ def MAP_score(source_id, target_labels, prediction):
     result['Translation'] = target_labels
     result['probabilities'] = [x[1] for x in prediction]
     # rank by the source_id and get the ranking for each of the queries for all the documents
-    result['rank'] = result.groupby('source_id')['probabilities'].rank(method='min', ascending=False)
+    result['rank'] = result.groupby('source_id')['probabilities'].rank(method='average', ascending=False)
     # create a new dataframe with only the right translations to get their rankings
     ranks = result[result['Translation'] == 1].reset_index()
     # compute the MAP score by first summing all inverses and dividing by the amount of queries
@@ -70,3 +71,35 @@ def feature_selection(model, scaler, trainset, testset, starting_features, added
             starting_features.append(feature)
             MapScore = MAP_score(testset['source_id'], target_test, prediction)
             print("Updated MAP score on test set with new feature {}: {:.4f}".format(feature, MapScore))
+
+
+def threshold_counts(s, threshold=0):
+    counts = s.value_counts(normalize=True, dropna=False)
+    if (counts >= threshold).any():
+        return False
+    return True
+    
+
+def downsample(imbalanced_data):
+    y = imbalanced_data["Translation"].astype(int)
+    y = np.where((y == 0), 0, 1)
+    
+    # Indicies of each class' observations
+    i_class0 = np.where(y == 0)[0]
+    i_class1 = np.where(y == 1)[0]
+
+    # Number of observations in each class
+    n_class0 = len(i_class0)
+    n_class1 = len(i_class1)
+    print("Class 0 size: {}".format(n_class0))
+    print("Class 1 size: {}".format(n_class1))
+
+    # For every observation of class 1, randomly sample from class 0 without replacement
+    i_class0_downsampled = np.random.choice(i_class0, size=n_class1, replace=False)
+    print("After Downsampling:")
+    print("Class 0 size: {}".format(len(i_class0_downsampled)))
+    print("Class 1 size: {}".format(n_class1))
+    index_balanced = i_class0_downsampled.tolist() + i_class1.tolist()
+    index_balanced = shuffle(index_balanced, random_state=42)
+    return imbalanced_data.iloc[index_balanced, :]
+            
